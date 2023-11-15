12a9592b098ccc9cc39f40ab7e11a14a53a7edda
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2023-11-15 16:20:20 +0100

    fini

--- Gemfile
+++ Gemfile
@@ -2,6 +2,12 @@ source "https://rubygems.org"
 
 ruby "3.1.2"
 
+# form with bootstrap
+gem "simple_form"
+
+# faker
+gem 'faker'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.2"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+Restaurant.destroy_all
+
+5.times do
+  restaurant = Restaurant.new
+  restaurant.name = Faker::Restaurant.name
+  restaurant.address = Faker::Address.street_address
+  restaurant.phone_number = Faker::PhoneNumber.phone_number
+  restaurant.category = 'italian'
+  restaurant.save!
+end
